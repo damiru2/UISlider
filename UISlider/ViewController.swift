679e4e1826cea2c319ab2c8e570c9b405eb8135e
damiru2 <chalkarov11@mail.ru>
2022-07-19 22:07:42 +0500

    created lil app

--- UISlider/ViewController.swift
+++ UISlider/ViewController.swift
@@ -9,44 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var label: UILabel!
-    var mySlider: UISlider!
-
+    
+    @IBOutlet weak var commonAmountLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var periodLabel: UILabel!
+    @IBOutlet weak var percentLabel: UILabel!
+    
+    @IBOutlet weak var amountSlider: UISlider!
+    @IBOutlet weak var periodSlider: UISlider!
+    @IBOutlet weak var percentSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        amountLabel.text = "10000 p."
+        periodLabel.text = "5 мес."
+        percentLabel.text = "5 %"
+        setAmountCount()
     
-        mySlider = UISlider(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
-        view.addSubview(mySlider)
-        mySlider.center = view.center
-        
-        mySlider.minimumValue = 0
-        mySlider.maximumValue = 100
-        mySlider.addTarget(self, action: #selector(sliderAction(sender:)), for: .valueChanged)
-        mySlider.isContinuous = false
-        
-        mySlider.minimumTrackTintColor = .red
-        mySlider.maximumTrackTintColor = .green
-        mySlider.thumbTintColor = .systemPink
-        
-        mySlider.value = 50
-        
-        label = UILabel(frame: CGRect(x: 0, y: 320, width: 300, height: 100))
-        view.addSubview(label)
-        label.text = "value"
-        label.center.x = view.center.x
-    }
-    
-    @objc func sliderAction(sender: UISlider) {
-        label.text = "\(sender.value)"
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        sleep(1)
-        mySlider.setValue(75.0, animated: true)
     }
 
-
+    @IBAction func amountSliderAction(_ sender: UISlider) {
+        switch sender.tag {
+        case 0: amountLabel.text = "\(Int(sender.value)) p."
+        case 1: periodLabel.text = "\(Int(sender.value)) мес."
+        case 2: percentLabel.text = "\(Int(sender.value)) %"
+        default:
+            return
+        }
+        
+        
+        setAmountCount()
+    }
+    
+    func setAmountCount() {
+        let percentAmount = amountSlider.value / 100 * percentSlider.value / 12
+        let periodAmount = percentAmount * periodSlider.value
+        commonAmountLabel.text = "\(amountSlider.value + periodAmount) p."
+    }
+    
 }
 
